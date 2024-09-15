e9e78925024543c2c35a5dc20567dc41d8b40c1d
lyf2000 <lyf2000@mail.ru>
2024-09-15 17:47:37 +0300

    user update

--- api/crud.py
+++ api/crud.py
@@ -1,10 +1,25 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from models import User
+from schema import UserIn
 
 
 def create_user(session: Session, user: User) -> User:
     session.add(user)
     session.commit()
     # session.refresh(user)  # not necessary, just for tip
+    return user
+
+
+def get_user_by_id(session: Session, user_id: int) -> User | None:
+    statement = select(User).filter(User.id == user_id).limit(1)
+    return session.execute(statement).scalars().one_or_none()
+
+
+def update_user(session: Session, user: User, user_in: UserIn) -> User:
+    for key, val in user_in.model_dump().items():  # TODO refact
+        setattr(user, key, val)
+    session.commit()
+
     return user
--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
 from sqlalchemy import select
 
 from db import SessionDep
 from models import User
-from crud import create_user as create_user_db
+from crud import create_user as create_user_db, get_user_by_id, update_user as update_user_db
 from schema import UserIn, UserOut
 
 
@@ -29,4 +30,15 @@ async def create_user(session: SessionDep, user: UserIn) -> UserOut:
     """create user"""
     user = User(**user.model_dump())
     create_user_db(session, user)
+    return UserOut.model_validate(user)
+
+
+@app.put("/users/{user_id}")
+async def update_user(session: SessionDep, user_id: int, user_in: UserIn) -> UserOut:
+    """update user"""
+    user = get_user_by_id(session, user_id)
+    if not user:
+        raise HTTPException("User not found", status_code=404)
+
+    user = update_user_db(session, user, user_in)
     return UserOut.model_validate(user)
